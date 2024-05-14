35e10cfc8099dc527d0256d8510c1d5ef8263f99
healey-o <150302176+healey-o@users.noreply.github.com>
2024-05-14 14:08:09 +1000

    Created a function that detects when the password input changes

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ app.height = 200
 #Create grid
 app.set_grid(3,2)
 
+#Define functions
+def on_text_change(event):
+    feedbackText.text = f"Your password is {str(len(passwordInput.text))} characters long."
 
 #Create widgets
 passwordLabel = gp.Label(app,"Enter Password:")
@@ -20,6 +23,8 @@ app.add(passwordLabel,1,1,align="center")
 app.add(passwordInput,1,2,fill=True)
 app.add(scoreDisplay,2,1,fill=True,column_span=2)
 app.add(feedbackText,3,1,fill=True,column_span=2)
+
+passwordInput.add_event_listener('change', on_text_change)
 
 
 app.run()
