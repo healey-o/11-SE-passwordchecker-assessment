67f7e65aad421bc5ebcc7090424812e9ae22a27d
healey-o <150302176+healey-o@users.noreply.github.com>
2024-05-16 11:45:24 +1000

    Fixed an bug where feedback text updated before the text to be checked had been updated

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ app.set_grid(4,2)
 
 #Define functions
 def on_text_change(event):
-    feedbackText.text = f"Length score: {passometer.score_length()}\nCharacter Score: {passometer.score_characters()}"
     passometer.update_password(passwordInput.text)
     scoreDisplay.value = passometer.combine_scores(50,50,0) #Weights to be adjusted
+    feedbackText.text = f"Length score: {passometer.score_length()}\nCharacter Score: {passometer.score_characters()}"
 
 #Create widgets
 #Title
