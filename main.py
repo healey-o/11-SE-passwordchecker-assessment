caede8b98331b39e43ee2625fd5598b6e4b96f6e
healey-o <150302176+healey-o@users.noreply.github.com>
2024-05-20 10:44:18 +1000

    Added 'help' and 'about' windows

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ app.resizable_vertical = False
 app.set_grid(5,2)
 
 #Define functions
+#Update screen to display score
 def on_text_change(event):
     if passwordInput.text != "":
         passometer.update_password(passwordInput.text)
@@ -26,7 +27,32 @@ Rarity Score: {passometer.score_rarity()}"""
         feedbackText.text = f"""Length score:
 Character Score:
 Rarity Score:"""
+        
+#Open subwindows
+def open_help(event):
+    helpWindow.show_on_top()
 
+def open_about(event):
+    aboutWindow.show_on_top()
+
+def close_help(event):
+    helpWindow.hide()
+
+def close_about(event):
+    aboutWindow.hide()
+
+#Create subwindows
+#Help
+helpWindow = gp.Window(app, 'Help')
+helpWindow.width = 400
+helpWindow.height = 300
+helpWindow.set_grid(3, 1)
+
+#About
+aboutWindow = gp.Window(app, 'About')
+aboutWindow.width = 400
+aboutWindow.height = 300
+aboutWindow.set_grid(3, 1)
 
 #Create widgets
 #Title
@@ -51,8 +77,27 @@ Rarity Score:"""
 btnContainer = gp.Container(app)
 btnContainer.set_grid(1,2)
 
-helpBtn = gp.Button(btnContainer,"Help",None)
-aboutBtn = gp.Button(btnContainer,"About",None)
+helpBtn = gp.Button(btnContainer,"Help",open_help)
+aboutBtn = gp.Button(btnContainer,"About",open_about)
+
+#Subwindow widgets
+#Help
+helpTitle = gp.StyleLabel(helpWindow,"About")
+helpTitle.font_size = 20
+helpTitle.font_name = "Times New Roman"
+
+helpText = gp.Label(helpWindow, "...")
+
+helpClose = gp.Button(helpWindow, "Close",close_help)
+
+#About
+aboutTitle = gp.StyleLabel(aboutWindow,"About")
+aboutTitle.font_size = 20
+aboutTitle.font_name = "Times New Roman"
+
+aboutText = gp.Label(aboutWindow, "...")
+
+aboutClose = gp.Button(aboutWindow, "Close",close_about)
 
 #Add widgets to grid
 app.add(title,1,1,align="center",column_span=2)
@@ -64,6 +109,14 @@ app.add(btnContainer,5,2,fill=True)
 
 btnContainer.add(helpBtn,1,1,fill=True)
 btnContainer.add(aboutBtn,1,2,fill=True)
+
+helpWindow.add(helpTitle,1,1,fill=True)
+helpWindow.add(helpText,2,1,fill=True,stretch=True)
+helpWindow.add(helpClose,3,1,valign="bottom")
+
+aboutWindow.add(aboutTitle,1,1,fill=True)
+aboutWindow.add(aboutText,2,1,fill=True,stretch=True)
+aboutWindow.add(aboutClose,3,1,valign="bottom")
 
 #Event listeners
 passwordInput.add_event_listener('change', on_text_change)
